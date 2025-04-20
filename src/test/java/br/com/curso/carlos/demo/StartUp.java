a94644ada4ba0496364177bd78580ac7885f366e
carlosRosa <crafaelrosa@gmail.com>
2025-04-20 19:44:31 -0300

    primeiro and point

---
+++ src/test/java/br/com/curso/carlos/demo/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.curso.carlos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUp {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
